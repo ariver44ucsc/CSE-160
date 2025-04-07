77cee59360aa4b66255637fd9a827cca2a0dc323
Andrew <ariver44@ucsc.edu>
2025-04-06 21:00:00 -0700

    commit

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -1,16 +1,33 @@
-// DrawTriangle.js (c) 2012 matsuda
-function main() {  
-  // Retrieve <canvas> element
-  var canvas = document.getElementById('example');  
-  if (!canvas) { 
+function main() {
+  var canvas = document.getElementById('example');
+  if (!canvas) {
     console.log('Failed to retrieve the <canvas> element');
-    return false; 
-  } 
+    return;
+  }
 
-  // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  // Clear the canvas with black background
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Create vector v1
+  let v1 = new Vector3([2.25, 2.25, 0]);
+
+  // Draw the vector
+  drawVector(v1, "red", ctx);
+}
+
+// Function to draw a vector from the center of the canvas
+function drawVector(v, color, ctx) {
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+
+  // Canvas center is (200, 200)
+  let originX = 200;
+  let originY = 200;
+
+  ctx.moveTo(originX, originY);
+  ctx.lineTo(originX + v.elements[0] * 20, originY - v.elements[1] * 20); // scale & invert y-axis
+  ctx.stroke();
 }
